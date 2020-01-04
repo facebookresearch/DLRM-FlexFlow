adfb1f76f30483b77aca4555587347256d098e34
Jongsoo Park <jongsoo@fb.com>
2020-01-04 08:40:13 -0800

    Point to a correct protobuf library (#5)
    Otherwise we get errors like
    ```
    dlrm_strategy.cc:(.text.startup+0xfa): undefined reference to `google::protobuf::internal::fixed_address_empty_string[abi:cxx11]'
    ```

--- src/runtime/gen_strategy.sh
+++ src/runtime/gen_strategy.sh
@@ -11,7 +11,7 @@ nembs=$2
 python3 dlrm_strategy.py -f dlrm_strategy.cc -g ${ngpus} -e ${nembs}
 
 echo "Compile..."
-g++ dlrm_strategy.cc strategy.pb.cc -o generator -std=c++11 -lprotobuf -L/usr/local/lib -I/usr/local/include -I${PROTOBUF}/src -pthread -O2
+g++ dlrm_strategy.cc strategy.pb.cc -o generator -std=c++11 -L${PROTOBUF}/src/.libs -lprotobuf -L/usr/local/lib -I/usr/local/include -I${PROTOBUF}/src -pthread -O2
 
 echo "Generate..."
 ./generator

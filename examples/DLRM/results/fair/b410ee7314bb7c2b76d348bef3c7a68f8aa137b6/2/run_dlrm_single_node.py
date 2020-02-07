cce6ca2884e2b0e7bb6566eacf2fb0dd763fb907
Yuchen Hao <haoyc@fb.com>
2020-02-07 12:14:17 -0800

    Add the script to allocate a single node in FC

--- examples/DLRM/results/fair/b410ee7314bb7c2b76d348bef3c7a68f8aa137b6/2/run_dlrm_single_node.py
+++ examples/DLRM/results/fair/b410ee7314bb7c2b76d348bef3c7a68f8aa137b6/2/run_dlrm_single_node.py
@@ -5,6 +5,10 @@ import itertools
 import subprocess
 import sys
 
+NUM_EMB_OPS = 8
+EMB_DIM = 64
+HASH_SIZE = 1000000
+NUM_INDICES = 1
 
 repeat = 1
 
@@ -15,22 +19,33 @@ num_gpus = [1, 2, 4, 8]
 with open("res_single_node.csv", "wb") as csvfile:
     writer = csv.writer(csvfile)
 
-    for gpus, gpu_batch_size in itertools.product(num_gpus, per_gpu_batch_sizes):
+    for gpu_batch_size, gpus in itertools.product(per_gpu_batch_sizes, num_gpus):
         for _ in range(repeat):
             strategy = "../../src/runtime/dlrm_strategy_emb_8_gpu_{}_node_1.pb".format(gpus)
-            batch_size = gpu_batch_size * gpus
 
+            batch_size = gpu_batch_size * gpus
             print("==== Running with {} gpus, batch size of {}, per-gpu batch size of {} ====".format(
-                gpus, batch_size, batch_size // gpus
+                gpus, batch_size, gpu_batch_size
             ))
+
+            emb_config = "-{}".format(HASH_SIZE) * NUM_EMB_OPS
+            emb_config = emb_config[1:]
 
             cmd = (
                 "./dlrm -ll:cpu 8 -ll:fsize 12000 -ll:zsize 20000 "
                 "--arch-sparse-feature-size 64 "
-                "--arch-embedding-size 1000000-1000000-1000000-1000000-1000000-1000000-1000000-1000000 "
-                "--arch-mlp-bot 64-512-512-64 --arch-mlp-top 576-1024-1024-1024-1 --epochs 20 "
-                "-dm:memorize -ll:gpu {gpus} -ll:util 12 -ll:dma 4 --batch-size {} --strategy {}"
-            ).format(batch_size, strategy, gpus=gpus)
+                "--arch-embedding-size {emb_config} "
+                "--arch-mlp-bot 64-512-512-64 "
+                "--arch-mlp-top 576-1024-1024-1024-1 "
+                "--epochs 20 --batch-size {batch_size} "
+                "-dm:memorize -ll:gpu {gpus} -ll:util 12 -ll:dma 4 "
+                "--strategy {strategy}"
+            ).format(
+                emb_config=emb_config,
+                batch_size=batch_size,
+                gpus=gpus,
+                strategy=strategy,
+            )
 
             try:
                 out = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)

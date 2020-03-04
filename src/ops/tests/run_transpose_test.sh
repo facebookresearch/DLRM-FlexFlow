9489c9a993b0bf795551a018898b832bffc352eb
Yubo Zhou <charles.zhou.6006@gmail.com>
2020-03-03 20:43:30 -0800

    put things together in run_test.sh

--- src/ops/tests/run_transpose_test.sh
+++ src/ops/tests/run_transpose_test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 numgpu="$1"
-
+python gen_test_case_transpose.py "$2" "$3" "$4"
 ./transpose_test -ll:gpu ${numgpu} -ll:cpu 4  -ll:util ${numgpu}  -dm:memorize --strategy ../../runtime/dlrm_strategy_emb_1_gpu_${numgpu}_node_1.pb
+python validate_transpose.py

8e9ecf10853119ed761a88263bdeb03170051693
Yubo Zhou <charles.zhou.6006@gmail.com>
2020-03-04 16:06:11 -0800

    add python test harness and batch_matmul tests

--- src/ops/tests/transpose_test.cc
+++ src/ops/tests/transpose_test.cc
@@ -7,14 +7,14 @@
 #define  PRECISION 16
 using namespace Legion;
 
+
 LegionRuntime::Logger::Category log_app("bmm_test");
 
 
-
-struct BMMTestMeta {
-  int m,k,n,d;
-  BMMTestMeta(int _m, int _k, int _n, int _d) {
-      m = _m, k = _k, n = _n, d = _d;
+struct TransposeTestMeta {
+  int m,k,d;
+  TransposeTestMeta(int _m, int _k, int _d) {
+      m = _m, k = _k, d = _d;
   }
 };
 
@@ -74,6 +74,7 @@ void initialize_tensor_from_file_task(const Task *task,
   {
     label_tensor_ptr[i] = a;
     i++;
+    // printf("%.6lf ", (float)tensor_ptr[i]);
   }   
 }
 
@@ -115,11 +116,11 @@ void dump_tensor_task(const Task* task,
   myfile.close();
 }
 
-BMMTestMeta get_test_meta(const std::string file_path) {
+TransposeTestMeta get_test_meta(const std::string file_path) {
   std::fstream myfile(file_path, std::ios_base::in);
-  int m,k,n,d;
-  myfile >> m >> k >> n >> d;
-  return BMMTestMeta(m,k,n,d);
+  int m,k,d;
+  myfile >> m >> k >> d;
+  return TransposeTestMeta(m,k,d);
 }
 
 void register_custom_tasks()
@@ -156,44 +157,31 @@ void top_level_task(const Task* task,
   }
   ffConfig.lg_ctx = ctx;
   ffConfig.lg_hlr = runtime;
+  ffConfig.profiling = false;
   ffConfig.field_space = runtime->create_field_space(ctx);
   // create ff model object
   FFModel ff(ffConfig);
   // create input tensor
-  Tensor dense_input1;
+  Tensor dense_input;
   {
-    const int dims[3] = {test_meta.d,test_meta.k,test_meta.m}; // target shape (d,k,m)
+    const int dims[3] = {test_meta.d,test_meta.m,test_meta.k}; // target shape (d,m,k)
     // HACK: have to pass "batch_matmul" 3-dimensional strategy string id to tell FF to distribute this tensor correctly 
-    dense_input1 = ff.create_tensor<3>(dims, "batch_matmul", DT_FLOAT);
+    dense_input = ff.create_tensor<3>(dims, "transpose", DT_FLOAT);
   }
-  Tensor dense_input2;
-  {
-    const int dims[3] = {test_meta.d,test_meta.k,test_meta.n}; // shape (n,k,d)
-    // HACK: have to pass "batch_matmul" 3-dimensional strategy string id to tell FF to distribute this tensor correctly 
-    dense_input2 = ff.create_tensor<3>(dims, "batch_matmul", DT_FLOAT);
-  }
-  // build batch matmul layer
-  Tensor batch_matmul_ret = ff.batch_matmul(
-      "batch_matmul", 
-      dense_input1, 
-      dense_input2, 
-      true /* trans_a */, 
-      false /* trans_b */);
+  // build transpose layer
+  Tensor ret = ff.transpose("transpose", dense_input);
   // load inputs tensors and output gradients tensors for testing
   auto input1_file_path = "test_input1.txt";
-  auto input2_file_path = "test_input2.txt";
   auto output_grad_file_path = "test_output_grad.txt";
-  initialize_tensor_from_file(input1_file_path, dense_input1, ff);
-  initialize_tensor_from_file(input2_file_path, dense_input2, ff);
-  initialize_tensor_gradient_from_file(output_grad_file_path, batch_matmul_ret, ff);
+  initialize_tensor_from_file(input1_file_path, dense_input, ff);
+  initialize_tensor_gradient_from_file(output_grad_file_path, ret, ff);
   // run forward and backward to produce results
   ff.init_layers();
   ff.forward();
   ff.backward();
   // dump results to file for python validation
-  dump_region_to_file(ff, batch_matmul_ret.region, "output.txt");
-  dump_region_to_file(ff, dense_input1.region_grad, "input1_grad.txt");
-  dump_region_to_file(ff, dense_input2.region_grad, "input2_grad.txt");
+  dump_region_to_file(ff, ret.region, "output.txt");
+  dump_region_to_file(ff, dense_input.region_grad, "input1_grad.txt");
 }
 
 

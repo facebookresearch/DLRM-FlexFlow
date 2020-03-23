02996a5ac5911b3b5ca6334756ff5c04c4d147ca
Yubo Zhou <charles.zhou.6006@gmail.com>
2020-03-23 16:38:28 -0700

    add reshape operator, debugging

--- src/runtime/model.cc
+++ src/runtime/model.cc
@@ -1233,17 +1233,17 @@ int main(int argc, char** argv)
   }
   // Reshape task
   {
-    TaskVariantRegistrar registrar(RESHAPE_2_TO_3_FWD_TASK_ID, "reshape_fwd_task");
+    TaskVariantRegistrar registrar(RESHAPE_3_TO_2_FWD_TASK_ID, "reshape_fwd_task");
     registrar.add_constraint(ProcessorConstraint(Processor::TOC_PROC));
     registrar.set_leaf();
-    Runtime::preregister_task_variant<Reshape2to3::forward_task>(
+    Runtime::preregister_task_variant<Reshape3to2::forward_task>(
         registrar, "reshape_fwd_task");
   }
   {
     TaskVariantRegistrar registrar(RESHAPE_3_TO_2_BWD_TASK_ID, "reshape_bwd_task");
     registrar.add_constraint(ProcessorConstraint(Processor::TOC_PROC));
     registrar.set_leaf();
-    Runtime::preregister_task_variant<Reshape2to3::backward_task>(
+    Runtime::preregister_task_variant<Reshape3to2::backward_task>(
         registrar, "reshape_bwd_task");
   }
 
